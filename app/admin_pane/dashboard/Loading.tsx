93c87cebd1a2f2383286cb8b4b6e5162bc11add8
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2023-08-30 23:00:39 -0500

    fix: included arcan favicon and updated loading screen

--- app/admin_pane/dashboard/Loading.tsx
+++ app/admin_pane/dashboard/Loading.tsx
@@ -1,7 +1,10 @@
+import "app/globals.css";
+
 export default function Loading() {
 	return (
-		<div className='w-full h-screen flex items-center flex-col justify-center'>
-			<h2 className='text-2xl font-bold'>Loading, please wait...</h2>
+		<div className="w-full h-screen flex items-center flex-col justify-center">
+		<h2 className="text-2xl font-bold">Loading, please wait...</h2>
+		<div className="loader"></div>
 		</div>
 	);
 };
