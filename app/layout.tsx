4da43af1a7d00ef603eedf577667d2efb0823cd5
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2023-09-02 12:20:26 -0500

    fix: moved header to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,36 @@
+import { Header } from '@/components/header'
+import { Providers } from '@/components/providers'
+import { fontMono, fontSans } from '@/lib/fonts'
+import { cn } from '@/lib/utils'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
 const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
   title: 'Arcan AI',
-  description: "A multiheaded modern data bridging package based on pipeline manifests to integrate between any modern (and old) data stack tools",
+  description:
+    'A multiheaded modern data bridging package based on pipeline manifests to integrate between any modern (and old) data stack tools'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head />
+      <body
+        className={cn(
+          'font-sans antialiased',
+          fontSans.variable,
+          fontMono.variable
+        )}
+      >
+        <Providers attribute="class" defaultTheme="system" enableSystem>
+        <Header />
+        <body className={inter.className}>{children}</body>
+        </Providers>
+      </body>
     </html>
   )
 }
-
-
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en">
-//       {/*
-//         <head /> will contain the components returned by the nearest parent
-//         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-//       */}
-//       <head />
-//       <body className={inter.className}>{children}
-//         {/* <AuthContextProvider> */}
-//           {children}
-//         {/* </AuthContextProvider> */}
-//       </body>
-//     </html>
-//   )
-// }
--- app/spells_studio/layout.tsx
+++ app/spells_studio/layout.tsx
@@ -1,13 +1,9 @@
-import { Metadata } from 'next'
-
-import { Toaster } from 'react-hot-toast'
-
 import '@/app/globals.css'
+import { Providers } from '@/components/providers'
 import { fontMono, fontSans } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
-import { TailwindIndicator } from '@/components/tailwind-indicator'
-import { Providers } from '@/components/providers'
-import { Header } from '@/components/header'
+import { Metadata } from 'next'
+// import { TailwindIndicator } from '@/components/tailwind-indicator'
 
 export const metadata: Metadata = {
   title: {
@@ -30,7 +26,7 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function ChatLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
@@ -41,14 +37,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
           fontMono.variable
         )}
       >
-        <Toaster />
+        {/* <Toaster /> */}
         <Providers attribute="class" defaultTheme="system" enableSystem>
           <div className="flex flex-col min-h-screen">
             {/* @ts-ignore */}
-            <Header />
+            {/* <Header /> */}
             <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
           </div>
-          <TailwindIndicator />
+          {/* <TailwindIndicator /> */}
         </Providers>
       </body>
     </html>
