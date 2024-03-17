1f99556a5e6ea5a9fd1a6d14e558c11ed81347e8
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2024-03-17 00:56:12 -0500

    fix: stabilized scaffold from play nextjs to set up fully featured frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,42 @@
-import { Header } from '@/components/header'
-import { Providers } from '@/components/providers'
-import { Inter } from 'next/font/google'
-import './globals.css'
-const inter = Inter({ subsets: ['latin'] })
+"use client";
 
-
-export const metadata = {
-  title: 'Arcan AI',
-  description: 'AI web3 tooling platform for the decentralized customization and enhancement of AI agents'
-}
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import "../styles/index.css";
+import "../styles/prism-vsc-dark-plus.css";
+import ToasterContext from "./api/contex/ToasetContex";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
       <head />
-      <body className={inter.className}>
-        
-        
-        <Providers attribute="class" defaultTheme="system" enableSystem>
 
-        <Header />
-        
-            <body className={inter.className}>{children}</body>
-        </Providers>
-        
+      <body>
+        <SessionProvider>
+          <ThemeProvider
+            attribute="class"
+            enableSystem={false}
+            defaultTheme="light"
+          >
+            <ToasterContext />
+            <Header />
+            {children}
+            <Footer />
+            <ScrollToTop />
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
-  )
+  );
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,40 @@
-"use client";
+import About from "@/components/About";
+import HomeBlogSection from "@/components/Blog/HomeBlogSection";
+import CallToAction from "@/components/CallToAction";
+import Clients from "@/components/Clients";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Faq from "@/components/Faq";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Team from "@/components/Team";
+import Testimonials from "@/components/Testimonials";
+import { getAllPosts } from "@/utils/markdown";
+import { Metadata } from "next";
 
-import { GithubLoginButton } from "@/components/login-button";
-import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
-// import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
-import Image from "next/image";
-import Link from "next/link";
-import { useActiveAccount } from "thirdweb/react";
-
-
-
-function Hero() {
-  const typewriter_words = [
-    {
-      text: "Build",
-    },
-    {
-      text: "Context",
-    },
-    {
-      text: "aware",
-    },
-    {
-      text: "AI",
-    },
-    {
-      text: "apps",
-    },
-    {
-      text: "with",
-    },
-    {
-      text: "Arcan.",
-      className: "relative z-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-blue-600 text-center font-sans font-bold",
-
-    },
-  ];
-  return (
-    <div className="flex flex-col items-center justify-center h-[40rem]  ">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-blue-700 before:blur-2xl before:opacity-10 before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-900 after:via-[#420aaa] after:blur-2xl after:opacity-40 after:content-[''] before:dark:bg-gradient-to-br before:dark:from-white before:dark:to-transparent before:dark:opacity-100 after:dark:from-sky-200 after:dark:via-blue-200 after:dark:opacity-100 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/logo_dark.png"
-          alt="arcan logo"
-          width={200}
-          height={120}
-        />
-      </div>
-      <TypewriterEffectSmooth words={typewriter_words} />
-      {/* <TextGenerateEffect words={'Build Context aware AI Apps with Arcan'} /> */}
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base h-[3rem]  ">
-        AI Web3 tooling platform for decentralized customization and enhancement of AI agents
-      </p>
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-      </div>
-      <GithubLoginButton />
-    </div>
-  );
-}
-
-
+export const metadata: Metadata = {
+  title: "Play Next.js - SaaS Starter Kit and Boilerplate for Next.js",
+  description: "Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
+};
 
 export default function Home() {
-  const activeAccount = useActiveAccount();
-  console.log(activeAccount);
+  const posts = getAllPosts(["title", "date", "excerpt", "coverImage", "slug"]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Check the Arcan API at&nbsp;
-          <Link href="https://api.arcanai.tech/">
-            <code className="font-mono font-bold">api/check</code>
-          </Link>
-        </p>
-
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <Link href="https://api.whatsapp.com/send/?phone=19853323941&text=Hi%20there!">
-            Chat with the Arcan Agent on WhatsApp 💬
-          </Link>
-        </p>
-
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          By {" "}
-          <p className="font-bold">
-            <a
-              href="https://broomva.tech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Broomva
-            </a>
-          </p>
-        </div>
-      </div>
+    <main>
+      <ScrollUp />
       <Hero />
-
+      <Features />
+      <About />
+      <CallToAction />
+      <Pricing />
+      <Testimonials />
+      <Faq />
+      <Team />
+      <HomeBlogSection posts={posts} />
+      <Contact />
+      <Clients />
     </main>
   );
 }
